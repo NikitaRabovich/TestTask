735b23b4d065b9b59c90c40874150159cb06aaba
Mikita Rabovich <MRabovich@gomel.iba.by>
2019-02-10 01:21:38 +0300

    10/02/19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  tagsForm = new FormControl('');
+  editTagsForm = new FormControl('');
+  displayedTags = [];
+  separators = new RegExp(/[\n,;]+/g);
+
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit() {
+    this.displayedTags = localStorage.getItem('tags').split(',');
+  }
+
+  addTags() {
+    var tags = this.filterNumeric(this.tagsForm.value.replace(/[^\S\n]/g, '').split(this.separators));
+    if (tags.length > 0) {
+      localStorage.setItem('tags', this.displayedTags.join() + ',' + tags.join());
+      this.displayedTags = localStorage.getItem('tags').split(',');
+    }
+  }
+
+  open(modal) {
+    this.modalService.open(modal, {ariaLabelledBy: 'modal-basic-title'});
+    this.editTagsForm.setValue(this.displayedTags);
+  }
+
+  editTags() {
+    var editedTags = this.filterNumeric(this.editTagsForm.value.toString().replace(/[^\S\n]/g, '').split(this.separators));
+    localStorage.removeItem('tags');
+    localStorage.setItem('tags', editedTags.join());    
+    this.displayedTags = localStorage.getItem('tags').split(',');
+  }
+
+  removeTag(i) {
+    this.displayedTags.splice(i, 1);
+    localStorage.removeItem('tags');
+    localStorage.setItem('tags', this.displayedTags.join());
+  }
+
+  filterNumeric(arr) {
+    return arr.filter(function(number) {
+      return !isNaN(number) && number;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
